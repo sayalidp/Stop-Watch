8846292e8733725e47258a921e513d863370c299
Sayali Parandwal <98018769+sayalidp@users.noreply.github.com>
2025-01-29 23:19:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,84 @@
+// import Card from "./assets/Card";
+
+// function App() {
+//   return(
+//     <>
+//       <Card></Card>
+//       <Card></Card>
+//       <Card></Card>
+//       <Card></Card>
+//     </>
+//   );
+// }
+
+// export default App----------------------------------------------------------------------
+
+
+// import Card from "./assets/Card";
+
+
+// function App() {
+//   return(
+//     <Card></Card>
+//   );
+// }
+
+// export dafault App-----------------------------------------------------------------------------
+// import Button from "./Button.jsx";
+
+// function App(){
+//   return(<Button/>);
+// }
+
+// export default App-----------------------------------------------------------------------------
+
+// import Student from "./assets/Student.jsx"
+
+// function App(){
+//   return(
+//     <>
+//       <Student name="Sayali" age = {15} isStudent= {true}/>
+//       <Student name = "Sejal" age = {16} isStudent= {false}/>
+//       <Student name="Samiksha" age = {17} isStudent= {true}/>
+//       <Student name="Bob" age={25} isStudent={false} />
+//       <Student name = "Rishie"/>
+//     </>
+//   )
+// }
+
+// export default App-------------------------------------------------------------------------
+
+// import UserGreeting from "./assets/Button/UserGreeting";
+
+// function App(){
+//   return(
+//     <>
+//       <UserGreeting isloggedIn = {true} username = "Sayali"/>
+//     </>
+//   )
+// }
+
+// export default App--------------------------------------------------------------------------
+
+// import List from './assets/List'
+
+// function App(){
+//   return(<List/>);
+// }
+
+// export default App--------------------------------------------------------------------------
+// import Button from "./Button.jsx"
+// import ProfiePicture from "./assets/ProfiePicture.jsx";
+
+// function App(){
+//   return(<ProfiePicture/>);
+// }
+
+// export default App--------------------------------------------------------------------------
+
+import SmallProj from './assets/SmallProj.jsx';
+
+function App(){
+  return(<SmallProj/>);
+}
+export default App
